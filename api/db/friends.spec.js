8953807dfbdcb0e239ebe4ec4d32eb478eb854f6
andr3w <liu.anray+github@gmail.com>
2018-09-14 22:08:38 -0400

    Create friend object structure

--- api/db/friends.spec.js
+++ api/db/friends.spec.js
@@ -17,19 +17,29 @@ describe('get', () => {
 
 test('add', async () => {
   const id = await accounts.create();
-  await friends.add(id, 'Grimlock');
+  const grimlock = {
+    name: 'Grimlock',
+    about: [ 'Likes to listen to stories.' ],
+    toTalkAbout: [ 'This new story I heard.' ],
+  };
+  await friends.add(id, grimlock);
   const count1 = await friends.count(id);
   expect(count1).toBe(1);
 
   const myFriends1 = await friends.get(id);
-  expect(myFriends1).toEqual(['Grimlock']);
+  expect(myFriends1).toEqual([ grimlock ]);
 
-  await friends.add(id, 'Swoop');
+  const swoop = {
+    name: 'Swoop',
+    about: [ 'Likes to fly.' ],
+    toTalkAbout: [ 'Where the Decepticons are.' ],
+  };
+  await friends.add(id, swoop);
   const count2 = await friends.count(id);
   expect(count2).toBe(2);
 
   const myFriends2 = await friends.get(id);
-  expect(myFriends2).toEqual(['Grimlock', 'Swoop']);
+  expect(myFriends2).toEqual([ grimlock, swoop ]);
 });
 
 test('add same name twice', async () => {
