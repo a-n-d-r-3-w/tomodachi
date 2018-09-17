5974f1b4374a57e36ed77f735b2a6bfaa700aadd
Andrew <andrew.liu01@libertymutual.com>
2018-09-17 15:38:06 -0400

    Add hard-coded friend

---
+++ public/main.js
@@ -0,0 +1,15 @@
+const addFriend = () =>
+  fetch('/api/friends', {
+    method: 'post',
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({
+      accountId: (new URL(location.href)).pathname.split('/')[2],
+      friend: {
+        name: 'Sludge',
+        about: [ 'Has a long neck' ],
+        toTalkAbout: [ 'Does he like mud?' ],
+      },
+    }),
+  });
