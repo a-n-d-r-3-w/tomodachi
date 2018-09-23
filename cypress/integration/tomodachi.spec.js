8829916a9a7f3bdd03abf75a7685ef236e3577f2
andr3w <liu.anray+github@gmail.com>
2018-09-22 20:33:48 -0400

    Extend Cypress test; show friend IDs

--- cypress/integration/tomodachi.spec.js
+++ cypress/integration/tomodachi.spec.js
@@ -6,4 +6,6 @@ it('Is tomodachi', () => {
   cy.get('[data-test-id="create-account"]').click();
   cy.url().should('include', '/account/')
   cy.get('[data-test-id="add-friend"]').click();
+  cy.get('[data-test-id="add-info"]').click();
+  cy.get('[data-test-id="next-row"]').should('be.visible');
 });
